0a050e0a24ca72aa55a901b49c2c434f76ba9361
Никита <nikitosmoroz21@gmail.com>
2022-12-13 23:04:56 +0300

    Commit 8. override overload

--- src/number4_Inheritance/InheritanceMain.java
+++ src/number4_Inheritance/InheritanceMain.java
@@ -5,6 +5,14 @@ public class InheritanceMain {
     public static void main(String[] args) {
         ElectricCar auto = new ElectricCar("VAS","LadaSedan", 90, 2);
         System.out.println(auto);
+
+        Auto auto1 = new Bus("Reno", "Bus", EngineType.PETROL, 100,200,0);
+        Auto auto2 = new Truck("Kamaz", "Mega", EngineType.DIESEL, 300,300,0);
+
+        auto1.start();
+        auto2.start();
+        System.out.println(auto1.isRunning());
+        System.out.println(auto2.isRunning());
     }
 
 }
