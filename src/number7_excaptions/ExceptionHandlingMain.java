17deb9a76b06e4412792d88b0a5a582894731b9b
Никита <nikitosmoroz21@gmail.com>
2022-12-16 21:21:40 +0300

    Commit 9. New part - Exception

---
+++ src/number7_excaptions/ExceptionHandlingMain.java
@@ -0,0 +1,12 @@
+package number7_excaptions;
+
+public class ExceptionHandlingMain {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static int divide(int numerator, int denominator) {
+        return numerator / denominator;
+    }
+}
