1ff8ea25b2024653a776884e82be53737b233fdb
Никита <morozov.nva@yandex.ru>
2022-12-28 23:44:11 +0300

    Commit 14. Set

---
+++ src/number8_collections/set/SetRunner.java
@@ -0,0 +1,8 @@
+package number8_collections.set;
+
+public class SetRunner {
+    public static void main(String[] args) {
+        String[] cars = {"Mini","Mercedes-Benz","Audi","VM"};
+        String[] otherCars = {"Audi", "Ford", "GMC", "Toyota"};
+    }
+}
