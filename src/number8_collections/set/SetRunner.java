f6e165e424316df9c631daf8bbb7a019979d0c45
Никита <morozov.nva@yandex.ru>
2022-12-29 21:29:24 +0300

    Commit 14. Set

--- src/number8_collections/set/SetRunner.java
+++ src/number8_collections/set/SetRunner.java
@@ -1,8 +1,25 @@
 package number8_collections.set;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetRunner {
     public static void main(String[] args) {
-        String[] cars = {"Mini","Mercedes-Benz","Audi","VM"};
-        String[] otherCars = {"Audi", "Ford", "GMC", "Toyota"};
+        String[] cars = {"Mini","Mercedes-Benz","Audi","VM", "Smart", "Toyota", "Porsche"};
+        String[] otherCars = {"Audi", "Ford", "GMC", "Toyota", "Chevrolet"};
+
+        Set<String> carSet = new HashSet<>(Arrays.asList(cars));
+
+        Set<String> otherCarSet = new HashSet<>(Arrays.asList(otherCars));
+
+        Set<String> uniqueCars = new HashSet<>(carSet);
+        uniqueCars.addAll(otherCarSet);
+        print(uniqueCars);
+
+    }
+
+    private static void print(Set<String> cars) {
+        System.out.println(cars);
     }
 }
