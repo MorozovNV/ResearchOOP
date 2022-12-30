7ce007bc5359fcb44a4c22715b943a386531dde3
Никита <morozov.nva@yandex.ru>
2022-12-30 23:38:39 +0300

    Commit 15. HashSet, equals

--- src/number8_collections/set/SetRunner.java
+++ src/number8_collections/set/SetRunner.java
@@ -6,20 +6,40 @@ import java.util.Set;
 
 public class SetRunner {
     public static void main(String[] args) {
-        String[] cars = {"Mini","Mercedes-Benz","Audi","VM", "Smart", "Toyota", "Porsche"};
+       /* String[] europaCars = {"Mini","Mercedes-Benz","Audi","VM", "Smart", "Toyota", "Porsche"};
         String[] otherCars = {"Audi", "Ford", "GMC", "Toyota", "Chevrolet"};
 
-        Set<String> carSet = new HashSet<>(Arrays.asList(cars));
+        Set<String> carSet = new HashSet<>(Arrays.asList(europaCars));
 
         Set<String> otherCarSet = new HashSet<>(Arrays.asList(otherCars));
 
         Set<String> uniqueCars = new HashSet<>(carSet);
         uniqueCars.addAll(otherCarSet);
-        print(uniqueCars);
+        print(uniqueCars);*/
+        // Это для урокво по set
 
+        Set<Car> sixCars = new HashSet<>();
+        sixCars.add(new Car("VM", "Golf", 45));
+        sixCars.add(new Car("VMa", "Golfa", 50));
+        sixCars.add(new Car("VMb", "Golfb", 55));
+        sixCars.add(new Car("VMc", "Golfc", 60));
+
+        Set<Car> europaCars = new HashSet<>();
+        europaCars.add(new Car("VM", "Golf", 45));
+        europaCars.add(new Car("VM1", "Golf1", 50));
+        europaCars.add(new Car("VM2", "Golf2", 55));
+        europaCars.add(new Car("VM3", "Golf3", 60));
+
+        Set<Car> uniqueCars = new HashSet<>(sixCars);
+        uniqueCars.addAll(europaCars);
+        print(uniqueCars);
     }
 
-    private static void print(Set<String> cars) {
+    private static void print(Set<Car   /*String*/> cars) {
+        System.out.printf("%-20s %-20s %-20s \n", "Car brand", "Model", "Price per day");
+        for (Car car : cars) {
+            System.out.printf("%-20s %-20s %-20s \n", car.getCarBrand(), car.getModel(), car.getPricePerDay());
+        }
         System.out.println(cars);
     }
 }
