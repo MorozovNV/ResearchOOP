2e6a5f5757cc786c1d003d23da9cfc3d6d5d2a3e
Никита <morozov.nva@yandex.ru>
2023-01-30 23:54:32 +0300

    Commit 18. HashMap

---
+++ src/number8_collections/map/HashMapLauncher.java
@@ -0,0 +1,32 @@
+package number8_collections.map;
+
+import javax.sound.midi.Soundbank;
+import java.util.*;
+
+public class HashMapLauncher {
+    public static void main(String[] args) {
+        Map<String, Integer> wordMap = new HashMap<>();
+        System.out.println("PLease enter some text");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        String[] tokens = string.split(" ");
+        for (String token: tokens) {
+            String word = token.toLowerCase();
+            Integer count = wordMap.get(word);
+            if (count == null) {
+                wordMap.put(word,1);
+            } else {
+                wordMap.put(word, count+1);
+            }
+        }
+        printMap(wordMap);
+
+    }
+
+    private static void printMap(Map<String, Integer> wordMap) {
+        Set<String> keys = wordMap.keySet();
+        for (String key: keys) {
+            System.out.printf("%-10s%-10s", key, wordMap.get(key));
+        }
+    }
+}
