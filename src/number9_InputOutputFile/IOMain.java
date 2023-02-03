c386ad8cbbb2a252e5d36feaf214ba82bf041329
Никита <morozov.nva@yandex.ru>
2023-02-03 23:51:37 +0300

    Commit 20. InputOutput

--- src/number9_InputOutputFile/IOMain.java
+++ src/number9_InputOutputFile/IOMain.java
@@ -1,7 +1,18 @@
 package number9_InputOutputFile;
 
+import number8_collections.treeMap.AverageStudentGrade;
+import number8_collections.treeMap.SubjectGrade;
+import number8_collections.treeMap.TreeMapRunner;
+
+import java.io.FileWriter;
+import java.util.NavigableMap;
+import java.util.Set;
+
 public class IOMain {
     public static void main(String[] args) {
+        NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades = TreeMapRunner.createGrades();
+
+        FileWriter writer = new FileWriter("GradeBook.txt");
 
     }
 }
